3ab7f1c9a86e28f0c2a23be5f27152c0d4d05a3c
Nicolas Pacheco <nicolaspacsim@gmail.com>
2026-05-05 20:58:54 +0200

    Creación de la base de datos y archivo conexion.php

---
+++ php/conexion.php
@@ -0,0 +1,15 @@
+<?php
+$servidor = "localhost";
+$usuario = "root";
+$password = "";
+$base_datos = "pistas_san_isidro";
+
+$conexion = new mysqli($servidor, $usuario, $password, $base_datos);
+
+// Comprobar si hay algún error
+if ($conexion->connect_error) {
+    die("Error de conexión: " . $conexion->connect_error);
+}
+
+// echo "Conexión exitosa a la base de datos";
+?>
